7af8732d6a04ea1dca3612bd6f65d72a0d97d8e8
Oscar Kemuel <oscar.kemuel5@gmail.com>
2022-07-05 00:22:44 -0300

    feat: add axios cache adapter

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,15 @@
 import axios from 'axios'
+import { setupCache } from 'axios-cache-adapter'
 
 export const api = axios.create({
   baseURL: 'http://localhost:3333',
 })
 
+const cache = setupCache({
+  maxAge: 15 * 60 * 1000
+})
+
 export const apiDictionary = axios.create({
   baseURL: 'https://api.dictionaryapi.dev/api/v2/entries/en/',
+  adapter: cache.adapter
 })
